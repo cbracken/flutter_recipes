30e441102f91e56a4d493ffbd0e2e789af30d056
Tong Wu <wutong@google.com>
2020-06-26 22:22:19 +0000

    [devicelab] Polish android sdk module.
    Bug: https://github.com/flutter/flutter/issues/53789
    Change-Id: I940efe2a1c83004e6a25fca8f815b327baf55518
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/3941
    Reviewed-by: Godofredo Contreras <godofredoc@google.com>
    Commit-Queue: Tong Wu <wutong@google.com>

--- recipe_modules/android_sdk/api.py
+++ recipe_modules/android_sdk/api.py
@@ -1,51 +1,51 @@
 # Copyright 2020 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 from contextlib import contextmanager
 from recipe_engine import recipe_api
 
-
 class AndroidSdkApi(recipe_api.RecipeApi):
-  """Provides Android SDK environment."""
+  """Provides Android SDK environment.
+
+  Expects the android29 cache to be mounted on the swarming bot.
+  """
 
   def install(self):
     """Installs Android SDK in cache."""
-    android_root = self.m.path['cache'].join('android_root')
-    with self.m.step.nest('Download Android SDK Components'):
+    with self.m.step.nest('download Android SDK components'):
       self.m.cipd.ensure(
-          android_root.join('platform-tools'),
+          self._root().join('platform-tools'),
           self.m.cipd.EnsureFile().add_package(
               'flutter/android/sdk/platform-tools/${platform}',
               'version:29.0.2',
           ),
       )
       self.m.cipd.ensure(
-          android_root.join('platforms'),
+          self._root().join('platforms'),
           self.m.cipd.EnsureFile().add_package(
               'flutter/android/sdk/platforms',
               'version:29r1',
           ),
       )
       self.m.cipd.ensure(
-          android_root.join('build-tools'),
+          self._root().join('build-tools'),
           self.m.cipd.EnsureFile().add_package(
               'flutter/android/sdk/build-tools/${platform}',
               'version:29.0.1',
           ),
       )
 
-    with self.m.step.nest('Android SDK Licenses'):
+    with self.m.step.nest('add Android SDK licenses'):
       self.m.file.ensure_directory('mkdir licenses',
-                                   android_root.join('licenses'))
+                                   self._root().join('licenses'))
       self.m.file.write_text(
           'android sdk license',
-          android_root.join('licenses', 'android-sdk-license'),
+          self._root().join('licenses', 'android-sdk-license'),
           str(self.m.properties['android_sdk_license']),
       )
       self.m.file.write_text(
           'android sdk preview license',
-          android_root.join('licenses', 'android-sdk-preview-license'),
+          self._root().join('licenses', 'android-sdk-preview-license'),
           str(self.m.properties['android_sdk_preview_license']),
       )
 
@@ -55,8 +55,10 @@ class AndroidSdkApi(recipe_api.RecipeApi):
 
     Must call api.android_sdk.install() first.
     """
-    android_root = self.m.path['cache'].join('android_root')
     with self.m.context(
-        env={'ANDROID_SDK_ROOT': android_root},
-        env_prefixes={'PATH': [android_root.join('platform-tools')]}):
+        env={'ANDROID_SDK_ROOT': self._root()},
+        env_prefixes={'PATH': [self._root().join('platform-tools')]}):
       yield
+
+  def _root(self):
+    return self.m.path['cache'].join('android29')
