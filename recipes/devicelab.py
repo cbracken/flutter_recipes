d1ca952b66d1a87197f160e848c72e3478461732
Tong Wu <wutong@google.com>
2020-07-06 03:46:15 +0000

    [devicelab] Enable running iOS tests.
    Bug: https://github.com/flutter/flutter/issues/53789
    Test: https://chromium-swarm.appspot.com/task?id=4d06817e0a2b4810
    Change-Id: Ia14c37829bafb60f867ab6fc721c585bf3ae788f
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/3940
    Commit-Queue: Tong Wu <wutong@google.com>
    Reviewed-by: Godofredo Contreras <godofredoc@google.com>

--- recipes/devicelab.py
+++ recipes/devicelab.py
@@ -6,6 +6,10 @@ from recipe_engine.recipe_api import Property
 
 DEPS = [
     'depot_tools/git',
+    'depot_tools/osx_sdk',
+    'flutter/android_sdk',
+    'flutter/repo_util',
+    'flutter/yaml',
     'recipe_engine/cipd',
     'recipe_engine/context',
     'recipe_engine/file',
@@ -13,10 +17,8 @@ DEPS = [
     'recipe_engine/path',
     'recipe_engine/platform',
     'recipe_engine/properties',
+    'recipe_engine/raw_io',
     'recipe_engine/step',
-    'android_sdk',
-    'repo_util',
-    'yaml',
 ]
 
 PROPERTIES = {
@@ -26,32 +28,58 @@ PROPERTIES = {
 
 def RunSteps(api, task_name):
   flutter_path = api.path['start_dir'].join('flutter')
-  api.repo_util.checkout(
-      'flutter',
-      flutter_path,
-      api.properties.get('git_url'),
-      api.properties.get('git_ref'),
-  )
-  api.step(
-      'flutter doctor', cmd=[flutter_path.join('bin', 'flutter'), 'doctor'])
-
-  # Reads the manifest.
-  devicelab_path = flutter_path.join('dev', 'devicelab')
-  manifest_yaml_path = devicelab_path.join('manifest.yaml')
-  result = api.yaml.read('read manifest', manifest_yaml_path, api.json.output())
-  manifest = result.json.output
-
-  # Verifies the manifest containing the task to run.
-  if task_name not in manifest['tasks']:
-    raise ValueError('Unknown task: %s' % task_name)
-
+  with api.step.nest('checkout flutter/flutter'):
+    api.repo_util.checkout(
+        'flutter',
+        flutter_path,
+        api.properties.get('git_url'),
+        api.properties.get('git_ref'),
+    )
   env, env_prefixes = api.repo_util.flutter_environment(flutter_path)
+  devicelab_path = flutter_path.join('dev', 'devicelab')
   with api.context(env=env, env_prefixes=env_prefixes, cwd=devicelab_path):
-    api.step('pub get', cmd=['pub', 'get'])
-    api.android_sdk.install()
-    with api.android_sdk.context():
-      api.step(
-          'run %s' % task_name, cmd=['dart', 'bin/run.dart', '-t', task_name])
+    api.step('flutter doctor', ['flutter', 'doctor'])
+
+    # Reads the manifest.
+    result = api.yaml.read('read manifest',
+                           devicelab_path.join('manifest.yaml'),
+                           api.json.output())
+    manifest = result.json.output
+
+    # Verifies that the manifest contains the task to run.
+    task = manifest['tasks'].get(task_name)
+    if not task:
+      raise ValueError('Unknown task: %s' % task_name)
+
+    # Example first capability values: linux/android, mac/ios.
+    first_capability = task['required_agent_capabilities'][0]
+    sdk = first_capability.split('/')[1]
+
+    # Runs a task.
+    api.step('pub get', ['pub', 'get'])
+    if sdk == 'android':
+      run_android_task(api, task_name)
+    elif sdk == 'ios':
+      run_ios_task(api, task_name)
+
+
+def run_android_task(api, task_name):
+  api.android_sdk.install()
+  with api.android_sdk.context():
+    api.step('run %s' % task_name, ['dart', 'bin/run.dart', '-t', task_name])
+
+
+def run_ios_task(api, task_name):
+  api.step('unlock login keychain', ['unlock_login_keychain.sh'])
+  # See go/googler-flutter-signing about how to renew the Apple development
+  # certificate and provisioning profile.
+  code_signing_env = {
+      'FLUTTER_XCODE_CODE_SIGN_STYLE': 'Manual',
+      'FLUTTER_XCODE_DEVELOPMENT_TEAM': 'S8QB4VV633',
+      'FLUTTER_XCODE_PROVISIONING_PROFILE_SPECIFIER': 'match Development *',
+  }
+  with api.context(env=code_signing_env):
+    api.step('run %s' % task_name, ['dart', 'bin/run.dart', '-t', task_name])
 
 
 def GenTests(api):
@@ -59,23 +87,47 @@ def GenTests(api):
       'missing_task_name',
       api.expect_exception('ValueError'),
   )
-  example_manifest = {"tasks": {"task1": {}, "task2": {}}}
+
+  sample_manifest = {
+      "tasks": {
+          "android_defines_test": {
+              "description": "Builds an APK with a --dart-define ...",
+              "stage": "devicelab",
+              "required_agent_capabilities": ["linux/android"],
+          },
+          "flavors_test": {
+              "description": "Checks that flavored builds work on Android.",
+              "stage": "devicelab",
+              "required_agent_capabilities": ["mac/android"],
+          },
+          "flutter_gallery_ios__compile": {
+              "description": "Collects various performance metrics of ...",
+              "stage": "devicelab_ios",
+              "required_agent_capabilities": ["mac/ios"],
+          },
+      },
+  }
+
   yield api.test(
       'unknown_task',
       api.properties(task_name='unknown_task'),
-      api.step_data('read manifest.parse', api.json.output(example_manifest)),
+      api.repo_util.flutter_environment_data(),
+      api.step_data('read manifest.parse', api.json.output(sample_manifest)),
       api.expect_exception('ValueError'),
   )
-  yield api.test(
-      'example_task',
-      api.properties(
-          git_ref='refs/pull/123/head', git_url='https://abc.com/repo'),
-      api.properties(
-          task_name='task1',
-          android_sdk_license='android_sdk_hash',
-          android_sdk_preview_license='android_sdk_preview_hash',
-      ),
-      api.repo_util.flutter_environment_data(),
-      api.step_data('read manifest.parse', api.json.output(example_manifest)),
-      api.post_check(lambda check, steps: check('run task1' in steps)),
-  )
+
+  for task_name in [
+      'android_defines_test', 'flavors_test', 'flutter_gallery_ios__compile'
+  ]:
+    yield api.test(
+        task_name,
+        api.properties(
+            git_ref='refs/pull/123/head', git_url='https://abc.com/repo'),
+        api.properties(
+            task_name=task_name,
+            android_sdk_license='android_sdk_hash',
+            android_sdk_preview_license='android_sdk_preview_hash',
+        ),
+        api.repo_util.flutter_environment_data(),
+        api.step_data('read manifest.parse', api.json.output(sample_manifest)),
+    )
