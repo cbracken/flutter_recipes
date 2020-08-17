e3974c4236a67c116f364cd15cc04e68390aa716
Godofredo Contreras <godofredoc@google.com>
2020-08-17 18:12:17 +0000

    Fix the logic to kill processes on windows.
    For windows we need to kill all the processes started by the task to
    ensure the task can cleanup all the created temporary folders. The
    existent logic was killing the processes but still raising and infra
    failure if the task failed to kill one of the commands.
    
    Change-Id: I9c7421b7a659fc27d753fa05b5bb907cfe083dd5
    Bug: https://github.com/flutter/flutter/issues/63699
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/5803
    Commit-Queue: Godofredo Contreras <godofredoc@google.com>
    Reviewed-by: Keyong Han <keyonghan@google.com>

--- recipe_modules/os_utils/api.py
+++ recipe_modules/os_utils/api.py
@@ -17,10 +17,7 @@ class FlutterDepsApi(recipe_api.RecipeApi):
       name(str): The name of the step.
       exe_name(str): The name of the windows executable.
     """
-    try:
-      step_result = self.m.step(name, ['taskkill', '/f', '/im', exe_name, '/t'])
-    except (self.m.step.StepFailure, self.m.step.InfraFailure):
-      pass
+    self.m.step(name, ['taskkill', '/f', '/im', exe_name, '/t'], ok_ret='any')
 
   def kill_win_processes(self):
     """Kills windows processes.
@@ -35,7 +32,11 @@ class FlutterDepsApi(recipe_api.RecipeApi):
     If it actually fails to kill the task, the job will just fail anyway.
     """
     if self.m.platform.is_win:
-      with self.m.step.nest('Killing Windows Processes'):
+      with self.m.step.nest('Killing Windows Processes') as presentation:
         self._kill_all('stop gradle daemon', 'java.exe')
         self._kill_all('stop dart', 'dart.exe')
         self._kill_all('stop adb', 'adb.exe')
+        self._kill_all('stop flutter_tester', 'flutter_tester.exe')
+        # Ensure we always pass this step as killing non existing processes
+        # may create errors.
+        presentation.status = 'SUCCESS'
