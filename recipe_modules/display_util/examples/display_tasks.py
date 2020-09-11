7c605b7aad0c8310d381d8c3936b46c910bcaca2
Godofredo Contreras <godofredoc@google.com>
2020-09-11 17:16:32 +0000

    Force autoroll.
    The autoroller has been blocked for a few days. We are forcing the roll
    to ensure the latest dependencies for the recipes are available.
    
    Change-Id: I6ab94edf546db4adfafc2e0f14ad6fbd8ebb1dbc
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/6400
    Reviewed-by: Tong Wu <wutong@google.com>
    Commit-Queue: Godofredo Contreras <godofredoc@google.com>

--- recipe_modules/display_util/examples/display_tasks.py
+++ recipe_modules/display_util/examples/display_tasks.py
@@ -13,16 +13,18 @@ DEPS = [
 
 def RunSteps(api):
   request = (
-      api.swarming.task_request().with_name(
-          "flutter_fuchsia_unittests").with_priority(100))
+      api.swarming.task_request().with_name("flutter_fuchsia_unittests"
+                                           ).with_priority(100)
+  )
   request = request.with_slice(
       0,
-      request[0].with_command([
-          "./run_tests.sh", "image_name", "packages_name"
-      ]).with_dimensions(pool="luci.flutter.tests").with_isolated(
-          "isolated_hash").with_expiration_secs(3600).with_io_timeout_secs(
-              3600).with_execution_timeout_secs(3600).with_idempotent(
-                  True).with_containment_type("AUTO"),
+      request[0].with_command(
+          ["./run_tests.sh", "image_name", "packages_name"]
+      ).with_dimensions(
+          pool="luci.flutter.tests"
+      ).with_isolated("isolated_hash").with_expiration_secs(3600)
+      .with_io_timeout_secs(3600).with_execution_timeout_secs(3600)
+      .with_idempotent(True).with_containment_type("AUTO"),
   )
   # Trigger the task request.
   metadata = api.swarming.trigger("Trigger Tests", requests=[request])
@@ -31,7 +33,8 @@ def RunSteps(api):
   fuchsia_output = api.path["cleanup"].join("fuchsia_test_output")
   api.file.ensure_directory("swarming output", fuchsia_output)
   results = api.swarming.collect(
-      "collect", metadata, output_dir=fuchsia_output, timeout="30m")
+      "collect", metadata, output_dir=fuchsia_output, timeout="30m"
+  )
   api.display_util.display_tasks(
       step_name="display tasks",
       results=results,
@@ -43,30 +46,38 @@ def RunSteps(api):
 def GenTests(api):
   yield (
       api.status_check.test("Test Infra Failure", status="infra_failure") +
-      api.step_data("Trigger Tests", api.swarming.trigger(["task1", "task2"])) +
       api.step_data(
+          "Trigger Tests", api.swarming.trigger(["task1", "task2"],
+                                                initial_id=0)
+      ) + api.step_data(
           "collect",
           api.swarming.collect([
               api.swarming.task_result(
-                  id="0", name="task1", state=api.swarming.TaskState.KILLED),
+                  id="0", name="task1", state=api.swarming.TaskState.KILLED
+              ),
               api.swarming.task_result(id="1", name="task2", failure=True),
           ]),
-      ))
+      )
+  )
   yield (
-      api.status_check.test("Test Failure", status="failure") +
-      api.step_data("Trigger Tests", api.swarming.trigger(["task1", "task2"])) +
-      api.step_data(
+      api.status_check.test("Test Failure", status="failure") + api.step_data(
+          "Trigger Tests", api.swarming.trigger(["task1", "task2"],
+                                                initial_id=0)
+      ) + api.step_data(
           "collect",
           api.swarming.collect([
               api.swarming.task_result(id="0", name="task1"),
               api.swarming.task_result(id="1", name="task2", failure=True),
           ]),
-      ))
-  yield (api.status_check.test("Test Success", status="success") +
-         api.step_data("Trigger Tests", api.swarming.trigger(["task1"])) +
-         api.step_data(
-             "collect",
-             api.swarming.collect([
-                 api.swarming.task_result(id="0", name="task1"),
-             ]),
-         ))
+      )
+  )
+  yield (
+      api.status_check.test("Test Success", status="success") + api.step_data(
+          "Trigger Tests", api.swarming.trigger(["task1"], initial_id=0)
+      ) + api.step_data(
+          "collect",
+          api.swarming.collect([
+              api.swarming.task_result(id="0", name="task1"),
+          ]),
+      )
+  )
