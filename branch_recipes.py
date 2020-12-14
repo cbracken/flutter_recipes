6ac5b4a18d39ad0464a5a87881b4171548a5dd72
Christopher Fujino <fujino@google.com>
2020-12-14 23:10:22 +0000

    Branch recipes for 1.25.x release
    Also delete 1.23.x and 1.24.x recipes. Update branch_recipes.py script to also
    delete.
    
    Change-Id: I9c8061d94a0ceb8741a3ad42a236ded4aed7bc6f
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/9220
    Reviewed-by: Keyong Han <keyonghan@google.com>
    Commit-Queue: Christopher Fujino <fujino@google.com>

--- branch_recipes.py
+++ branch_recipes.py
@@ -3,6 +3,7 @@
 import getopt
 import os
 import re
+import shutil
 import subprocess
 import sys
 
@@ -13,6 +14,7 @@ RECIPES_TO_BRANCH = (
         'engine/engine_metrics',
         'engine/scenarios',
         'engine/web_engine_framework',
+        'engine/web_engine_drone',
         'engine',
         'engine_builder',
         'femu_test',
@@ -33,14 +35,15 @@ RECIPES_TO_SKIP = (
         )
 
 repo_root = os.path.dirname(os.path.realpath(__file__))
-recipes_dir = os.path.join(repo_root, 'recipes')
+RECIPES_DIR = os.path.join(repo_root, 'recipes')
 
-def usage(optional_options, required_options):
+def usage(optional_options, required_options, exit_code=0):
+    """Print script usage and exit."""
     print('A command-line tool for generating legacy recipes for release ' +
             'branches.\n')
     print('Usage: ./branch_recipes.py --flutter-version=<flutter version string> ' +
             '--recipe-revision=<recipes git hash>\n')
-    print('Where --flutter-version is of the form x_y_z and represents the ' +
+    print('Where --flutter-version is of the form x_y_0 and represents the ' +
             'stable release\nthe branch in question is a candidate for, and '
             '--recipe-revision is the recipes\nrepo revision at the time the '
             'release branch was branched off of master.\n')
@@ -50,71 +53,110 @@ def usage(optional_options, required_options):
     print('\nOptional options:')
     for opt in optional_options:
         print('  --' + opt)
+    sys.exit(exit_code)
 
 def parse_arguments(argv):
+    """Parse and validate command line arguments."""
     options = {
             'force': False,
+            'delete': False,
             }
     try:
-        optional_options = ('force', 'help')
+        optional_options = ('force', 'help', 'delete')
         required_options = ('flutter-version=', 'recipe-revision=')
-        opts, args = getopt.getopt(argv, '', optional_options + required_options)
+        opts, _args = getopt.getopt(argv, '', optional_options + required_options)
     except getopt.GetoptError:
-        usage(optional_options, required_options)
-        sys.exit(1)
+        print('Error parsing arguments!\n')
+        usage(optional_options, required_options, 1)
     for opt, arg in opts:
         if opt == '--help':
-            usage(optional_options, required_options)
-            sys.exit(0)
+            usage(optional_options, required_options, 0)
         elif opt == '--force':
             options['force'] = True
         elif opt == '--flutter-version':
-            if not re.search(r'^\d+_\d+_\d+$', arg):
+            if not re.search(r'^\d+_\d+_0+$', arg):
                 print('Error! Invalid value passed to --flutter-version: "%s"' %
                         arg)
-                print('It should be of the form x_y_z')
+                print('It should be of the form x_y_0')
                 sys.exit(1)
             options['flutter-version='] = arg
         elif opt == '--recipe-revision':
             if not re.search(r'^[0-9a-z]{40}$', arg):
-                print('Error! Invalid value passed to --flutter-version: "%s"' %
+                print('Error! Invalid value passed to --recipe-revision: "%s"' %
                         arg)
                 print('It should be a valid git hash')
                 sys.exit(1)
             options['recipe-revision='] = arg
-    for required_opt in required_options:
-        if options.get(required_opt, None) is None:
-            usage(optional_options, required_options)
-            sys.exit(1)
+        elif opt == '--delete':
+            options['delete'] = True
+    # validate
+    if options['delete']:
+        if 'flutter-version=' not in options:
+            print('meep')
+            print(options)
+            usage(optional_options, required_options, 1)
+    else:
+        if 'flutter-version=' not in options or 'recipe-revision=' not in options:
+            print('moop')
+            print(options)
+            usage(optional_options, required_options, 1)
     return options
 
-def get_all_recipes(cwd):
-    recipe_pattern = r'\.py$'
-    forked_recipe_pattern = r'_\d+_\d+_\d+\.py$'
-    expectation_pattern = r'\.expected$'
-    accumulated_files = []
-    for root, dirs, files in os.walk(cwd):
-        for filename in files:
-            if (re.search(recipe_pattern, filename) and not
-                re.search(forked_recipe_pattern, filename)):
-                accumulated_files.append(os.path.join(root, filename))
-        for dir in dirs:
-            if not re.search(expectation_pattern, dir):
-                accumulated_files += get_all_recipes(os.path.join(root, dir))
-    return accumulated_files
+def get_recipes(working_directory):
+    """Returns the paths to recipes and expectation file directories.
 
-def contains(subject, prefix, tuple_of_candidates):
+    Args:
+        working_directory (str): absolute path to the directory where the recipes
+            are located.
+
+    Returns:
+        latest_recipes (str[]): paths to all unbranched recipes.
+        branched_recipes (str[]): paths to all branched recipes.
+        branched_expectations (str[]): paths to all expectation directories of
+            branches recipes.
+    """
+    recipe_pattern = r'\.py$'
+    branched_recipe_pattern = r'_\d+_\d+_\d+\.py$'
+    expectation_pattern = r'\.expected$'
+    latest_recipes = []
+    branched_recipes = []
+    branched_expectations = []
+    for root, dirs, files in os.walk(working_directory):
+        for filename in files:
+            if (re.search(recipe_pattern, filename)):
+                if re.search(branched_recipe_pattern, filename):
+                    branched_recipes.append(os.path.join(root, filename))
+                else:
+                    latest_recipes.append(os.path.join(root, filename))
+        for dir_name in dirs:
+            if re.search(expectation_pattern, dir_name):
+                branched_expectations.append(os.path.join(root, dir_name))
+    return latest_recipes, branched_recipes, branched_expectations
+
+def contains(file_path, prefix, tuple_of_candidates):
+    """Given the full path to a file, returns the recipe sub-string.
+
+    If in the supplied tuple of candidates
+
+    Args:
+        file_path (str): Path to the file to look up.
+        prefix (str): Absolute path to directory containing all recipes.
+        tuple_of_candidates (str()): Tuple containing expected sub-strings.
+            Return None if file_path does not map to one of these.
+
+    Returns (str | None): recipe sub-string if the file is contained in the
+        provided candidates, else None.
+    """
     for candidate in tuple_of_candidates:
-        if subject == os.path.join(prefix, candidate + r'.py'):
+        if file_path == os.path.join(prefix, candidate + r'.py'):
             return candidate
     return None
 
-def main(argv):
-    options = parse_arguments(sys.argv[1:])
-
-    recipes = get_all_recipes(recipes_dir)
-    for recipe in recipes:
-        recipe_sub_string = contains(recipe, recipes_dir, RECIPES_TO_BRANCH)
+def branch_recipes(options):
+    """Branch all latest recipes on disk."""
+    latest_recipes, _branched_recipes, _branched_expectations = get_recipes(RECIPES_DIR)
+    for recipe in latest_recipes:
+        recipe_sub_string = contains(recipe, RECIPES_DIR, RECIPES_TO_BRANCH)
         if recipe_sub_string is not None:
             print('Reading file %s from revision %s' % (recipe,
                 options['recipe-revision=']))
@@ -125,9 +167,9 @@ def main(argv):
                         'show',
                         '%s:./%s' % (options['recipe-revision='], recipe_sub_string + r'.py'),
                         ],
-                    cwd=recipes_dir,
+                    cwd=RECIPES_DIR,
                     ).decode('utf-8')
-            new_file_path = '%s/%s_%s.py' % (recipes_dir, recipe_sub_string,
+            new_file_path = '%s/%s_%s.py' % (RECIPES_DIR, recipe_sub_string,
                     options['flutter-version='])
             if os.path.exists(new_file_path):
                 if options['force']:
@@ -141,6 +183,30 @@ def main(argv):
                 print('Writing %s\n' % new_file_path)
                 new_file.write(code)
         else:
-            assert contains(recipe, recipes_dir, RECIPES_TO_SKIP), 'Expected %s to be branched or skipped.' % recipe
+            assert contains(recipe, RECIPES_DIR, RECIPES_TO_SKIP), 'Expected %s to be branched or skipped.' % recipe
+
+def delete_recipes(options):
+    """Delete branched recipes and expectation directories of a given version."""
+    _latest_recipes, branched_recipes, branched_expectations = get_recipes(RECIPES_DIR)
+    branched_recipes.sort()
+    for recipe in branched_recipes:
+        suffix = options['flutter-version='] + r'.py'
+        if recipe.endswith(suffix):
+            print('Deleting file %s' % recipe)
+            os.remove(recipe)
+    for expectations in branched_expectations:
+        suffix = options['flutter-version='] + r'.expected'
+        if expectations.endswith(suffix):
+            print('Deleting directory %s' % expectations)
+            shutil.rmtree(expectations)
+
+
+def main(argv):
+    options = parse_arguments(sys.argv[1:])
+
+    if options['delete']:
+        delete_recipes(options)
+    else:
+        branch_recipes(options)
 
 main(sys.argv[1:])
